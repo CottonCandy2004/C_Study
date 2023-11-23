d05e9ae759efbc66db6d3456d35e84f7684157a0
CottonCandy2004 <805909642@qq.com>
2023-11-23 18:12:28 +0800

    update#2

---
+++ 05/5002.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void ball(int n,double *p,double *s) //使用指针带出2个数据
+{
+    if (n==0)
+    {
+        *s=100;
+    }
+    else
+    {
+        ball(n-1,p,s);
+        *s/=2.0;
+        *p+=3*(*s);
+    }
+}
+
+int main()
+{
+    int n;
+    double sum=0,last=0;
+    double *p=&sum,*s=&last;
+    scanf("%d",&n);
+    ball(n,p,s);
+    printf("%.4f,%.4f",*s,*p);
+}
